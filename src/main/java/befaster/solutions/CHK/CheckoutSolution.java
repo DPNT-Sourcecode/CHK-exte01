d762e38668d9cb68ca2d7f1b188f251940705fac
sbx_user1061 <sbx_user1061@ip-10-12-55-178.eu-west-2.compute.internal>
2018-07-05 09:10:08 +0000

    Thu Jul 05 09:10:08 UTC 2018

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -5,8 +5,14 @@ import befaster.runner.SolutionNotImplementedException;
 public class CheckoutSolution {
     public Integer checkout(String skus) {
         
-        String [] items = new String[100];
+        String [] items = new String[1000];
         items = skus.split(" ");
-        return 0;
+        Integer [] prices = new Integer[1000];
+        
+        for (int i = 0; i < items.length; i++) {
+            if (str.equals("A")) {
+                prices[]
+            }
+        }
     }
 }
