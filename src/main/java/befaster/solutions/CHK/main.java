611f37321d123fbb56fe88197746453c72cd357e
sbx_user1061 <sbx_user1061@ip-10-12-55-178.eu-west-2.compute.internal>
2018-07-05 09:30:39 +0000

    Thu Jul 05 09:30:39 UTC 2018

---
+++ src/main/java/befaster/solutions/CHK/main.java
@@ -0,0 +1,15 @@
+package befaster.solutions.CHK;
+
+import java.util.*;
+import befaster.runner.SolutionNotImplementedException;
+
+public class Main {
+
+    public static void main(String [] args) {
+
+        CheckoutSolution s = new CheckoutSolution();
+        Integer a = s.checkout("A");
+        System.out.println(a);
+    }
+    
+}
