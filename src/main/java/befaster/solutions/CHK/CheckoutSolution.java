80e6027e81f548a1950f320d47b7b790779e36d6
sbx_user1061 <sbx_user1061@ip-10-12-248-210.eu-west-2.compute.internal>
2018-07-07 17:39:39 +0000

    Sat Jul 07 17:39:39 UTC 2018

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -130,19 +130,164 @@ public class CheckoutSolution {
 
         return totalCheckout;
     }
+    
+    public class Item {
 
-   public class Item {
+        char type;
+        int value;
+        int appearances;
 
-    char type;
-    int value;
-    int appearances;
+        public Item(char type, int value) {
+            this.type = type;
+            this.value = value;
+            this.appearances = 0;
+        }
 
-    public Item(char type, int value) {
-        this.type = type;
-        this.value = value;
-        this.appearances = 0;
+    }
+
+    public class SpecialOffer {
+
+    public int itemAOffer(int price, int itemsNumber) {
+
+        while (itemsNumber >= 5) {
+            price -= 50;
+            itemsNumber -= 5;
+        }
+
+        while (itemsNumber >= 3) {
+            price -= 20;
+            itemsNumber -= 3;
+        }
+        return price;
+    }
+
+    public int itemBOffer(int price, int itemsNumber) {
+
+        while (itemsNumber >= 2) {
+            price -= 15;
+            itemsNumber -= 2;
+        }
+        return price;
+    }
+
+    public int itemEOffer(int price, int itemsNumber, int appearancesB) {
+
+        while (itemsNumber >= 2 && appearancesB > 0) {
+            price -= 30;
+            itemsNumber -= 2;
+            appearancesB--;
+        }
+        return price;
+    }
+
+    public int itemFOffer(int price, int itemsNumber) {
+
+        while (itemsNumber >= 3) {
+            price -= 10;
+            itemsNumber -= 3;
+        }
+        return price;
+    }
+
+    public int itemKOffer(int price, int itemsNumber) {
+
+        while (itemsNumber >= 2) {
+            price -= 10;
+            itemsNumber -= 2;
+        }
+        return price;
+    }
+
+    public int itemNOffer(int price, int itemsNumber, int appearancesM) {
+
+        while (itemsNumber >= 3 && appearancesM > 0) {
+            price -= 15;
+            itemsNumber -= 3;
+            appearancesM--;
+        }
+        return price;
+    }
+
+    public int itemPOffer(int price, int itemsNumber) {
+
+        while (itemsNumber >= 5) {
+            price -= 50;
+            itemsNumber -= 5;
+        }
+        return price;
+    }
+
+    public int itemQOffer(int price, int itemsNumber) {
+
+        while (itemsNumber >= 3) {
+            price -= 10;
+            itemsNumber -= 3;
+        }
+        return price;
+    }
+
+    public int itemROffer(int price, int itemsNumber, int appearancesQ) {
+
+        while (itemsNumber >= 3 && appearancesQ > 0) {
+            price -= 30;
+            itemsNumber -= 3;
+            appearancesQ--;
+        }
+        return price;
+    }
+
+    public int itemUOffer(int price, int itemsNumber) {
+
+        while (itemsNumber >= 4) {
+            price -= 40;
+            itemsNumber -= 4;
+        }
+        return price;
+    }
+
+    public int itemVOffer(int price, int itemsNumber) {
+
+        while (itemsNumber >= 3) {
+            price -= 20;
+            itemsNumber -= 3;
+        }
+
+        while (itemsNumber >= 2) {
+            price -= 10;
+            itemsNumber -=2;
+        }
+        return price;
+    }
+
+
+    public int getSpecialOffer(char itemType, int price, int itemsNumber, int appearances){
+        if (itemType == 'A') {
+            return itemAOffer(price,itemsNumber);
+        } else if (itemType == 'B') {
+            return itemBOffer(price, itemsNumber);
+        } else if (itemType == 'E') {
+            return itemEOffer(price,itemsNumber, appearances);
+        } else if (itemType == 'F') {
+            return itemFOffer(price, itemsNumber);
+        } else if (itemType == 'K') {
+            return itemKOffer(price, itemsNumber);
+        } else if (itemType == 'N') {
+            return itemNOffer(price,itemsNumber, appearances);
+        } else if (itemType == 'P') {
+            return itemPOffer(price, itemsNumber);
+        } else if (itemType == 'Q') {
+            return itemQOffer(price, itemsNumber);
+        } else if (itemType == 'R') {
+            return itemROffer(price, itemsNumber, appearances);
+        } else if (itemType == 'U') {
+            return itemUOffer(price, itemsNumber);
+        } else if (itemType == 'V') {
+            return itemVOffer(price, itemsNumber);
+        }
+        return price;
     }
 
 }
 
 }
+
