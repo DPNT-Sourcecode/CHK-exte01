6c69b24aba7ee6cee24fdb2f56df8b59a53526e6
sbx_user1061 <sbx_user1061@ip-10-12-248-210.eu-west-2.compute.internal>
2018-07-07 16:12:52 +0000

    Sat Jul 07 16:12:52 UTC 2018

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -47,12 +47,12 @@ public class CheckoutSolution {
         items.add(new Item('Z',50));
 
 
-        f
-        Iterator<Item> itr = items.iterator();
-        while (itr.hasNext()) {
-            for (int i = 0; i < skus.length(); i++) {
+        for (int i = 0; i < skus.length(); i++) {
+            Iterator<Item> itr = items.iterator();
+            while (itr.hasNext()) {
                 if (skus.charAt(i) == itr.next().type) {
                     itr.next().appearances++;
+                    break;
                 }
             }
         }
@@ -60,13 +60,18 @@ public class CheckoutSolution {
 
     public Integer checkout(String skus) {
         
-        int size = skus.length();
-        Integer [] prices = new Integer[size];
-        Integer [] items = new Integer[26];
+        // int size = skus.length();
+        // Integer [] prices = new Integer[size];
+        // Integer [] items = new Integer[26];
 
-        for (int i = 0; i < 26; i++) {
-            items[i] = 0;
-        }
+        // for (int i = 0; i < 26; i++) {
+        //     items[i] = 0;
+        // }
+
+        initialize(skus);
+
+        Iterator<Item> itr = items.iterator();
+        while (itr.hasNext())
         
         if (skus.length() == 0) {
             return 0;
@@ -76,133 +81,134 @@ public class CheckoutSolution {
             return -1;
         }
         
-        // int countA = 0;
-        // int countB = 0;
-        // int countE = 0;
-        // int countF = 0;
+        // // int countA = 0;
+        // // int countB = 0;
+        // // int countE = 0;
+        // // int countF = 0;
 
-        for (int i = 0; i < skus.length(); i++) {
-            if (skus.charAt(i) == 'A') {
-                prices[i] = 50;
-                items[0]++;
-            } else if (skus.charAt(i) == 'B') {
-                prices[i] = 30;
-                items[1]++;
-            } else if (skus.charAt(i) == 'C') {
-                prices[i] = 20;
-            } else if (skus.charAt(i) == 'D') {
-                prices[i] = 15;
-            } else if (skus.charAt(i) == 'E') {
-                prices[i] = 40;
-                items[4]++;
-            } else if (skus.charAt(i) == 'F') {
-                prices[i] = 10;
-                items[5]++;
-            } else if (skus.charAt(i) == 'G') {
-                prices[i] = 20;
-            } else if (skus.charAt(i) == 'H') {
-                prices[i] = 10;
-                items[7]++;
-            } else if (skus.charAt(i) == 'I') {
-                prices[i] = 35;
-            } else if (skus.charAt(i) == 'J') {
-                prices[i] = 60;
-            } else if (skus.charAt(i) == 'K') {
-                prices[i] = 80;
-                items[10]++;
-            } else if (skus.charAt(i) == 'L') {
-                prices[i] = 90;
-            } else if (skus.charAt(i) == 'M') {
-                prices[i] = 15;
-            } else if (skus.charAt(i) == 'N') {
-                prices[i] = 40;
-                items[13]++;
-            } else if (skus.charAt(i) == 'O') {
-                prices[i] = 10;
-            } else if (skus.charAt(i) == 'P') {
-                prices[i] = 50;
-                items[15]++;
-            } else if (skus.charAt(i) == 'Q') {
-                prices[i] = 30;
-                items[16]++;
-            } else if (skus.charAt(i) == 'R') {
-                prices[i] = 50;
+        // for (int i = 0; i < skus.length(); i++) {
+        //     if (skus.charAt(i) == 'A') {
+        //         prices[i] = 50;
+        //         items[0]++;
+        //     } else if (skus.charAt(i) == 'B') {
+        //         prices[i] = 30;
+        //         items[1]++;
+        //     } else if (skus.charAt(i) == 'C') {
+        //         prices[i] = 20;
+        //     } else if (skus.charAt(i) == 'D') {
+        //         prices[i] = 15;
+        //     } else if (skus.charAt(i) == 'E') {
+        //         prices[i] = 40;
+        //         items[4]++;
+        //     } else if (skus.charAt(i) == 'F') {
+        //         prices[i] = 10;
+        //         items[5]++;
+        //     } else if (skus.charAt(i) == 'G') {
+        //         prices[i] = 20;
+        //     } else if (skus.charAt(i) == 'H') {
+        //         prices[i] = 10;
+        //         items[7]++;
+        //     } else if (skus.charAt(i) == 'I') {
+        //         prices[i] = 35;
+        //     } else if (skus.charAt(i) == 'J') {
+        //         prices[i] = 60;
+        //     } else if (skus.charAt(i) == 'K') {
+        //         prices[i] = 80;
+        //         items[10]++;
+        //     } else if (skus.charAt(i) == 'L') {
+        //         prices[i] = 90;
+        //     } else if (skus.charAt(i) == 'M') {
+        //         prices[i] = 15;
+        //     } else if (skus.charAt(i) == 'N') {
+        //         prices[i] = 40;
+        //         items[13]++;
+        //     } else if (skus.charAt(i) == 'O') {
+        //         prices[i] = 10;
+        //     } else if (skus.charAt(i) == 'P') {
+        //         prices[i] = 50;
+        //         items[15]++;
+        //     } else if (skus.charAt(i) == 'Q') {
+        //         prices[i] = 30;
+        //         items[16]++;
+        //     } else if (skus.charAt(i) == 'R') {
+        //         prices[i] = 50;
 
-            }
-        }
+        //     }
+        // }
 
-        while (countE >= 2 && countB != 0) {
-            for (int i = 0; i < size; i++) {
-                if (prices[i] == 30) {
-                    prices[i] = 0;
-                    countE -= 2;
-                    countB--;
-                    break;
-                }
-            }
-        }
+        // while (countE >= 2 && countB != 0) {
+        //     for (int i = 0; i < size; i++) {
+        //         if (prices[i] == 30) {
+        //             prices[i] = 0;
+        //             countE -= 2;
+        //             countB--;
+        //             break;
+        //         }
+        //     }
+        // }
 
-            while (countA >= 5) {
-                int removed = 0;
-                for (int i = 0; i < size; i++) {
-                    if (prices[i] == 50) {
-                        prices[i] = 0;
-                        countA--;
-                        removed++;
-                    }
-                    if (removed == 5) {
-                        prices[i] = 200;
-                        break;
-                    }
-                }
-            }
+        //     while (countA >= 5) {
+        //         int removed = 0;
+        //         for (int i = 0; i < size; i++) {
+        //             if (prices[i] == 50) {
+        //                 prices[i] = 0;
+        //                 countA--;
+        //                 removed++;
+        //             }
+        //             if (removed == 5) {
+        //                 prices[i] = 200;
+        //                 break;
+        //             }
+        //         }
+        //     }
 
-            while (countA >= 3) {
-                int removed = 0;
-                for (int i = 0; i < size; i++) {
-                    if (prices[i] == 50) {
-                        prices[i] = 0;
-                        countA--;
-                        removed++;
-                    }
-                    if (removed == 3) {
-                        prices[i] = 130;
-                        break;
-                    }
-                }
-            }
+        //     while (countA >= 3) {
+        //         int removed = 0;
+        //         for (int i = 0; i < size; i++) {
+        //             if (prices[i] == 50) {
+        //                 prices[i] = 0;
+        //                 countA--;
+        //                 removed++;
+        //             }
+        //             if (removed == 3) {
+        //                 prices[i] = 130;
+        //                 break;
+        //             }
+        //         }
+        //     }
 
-            while (countB >= 2) {
-                int removed = 0;
-                for (int i = 0; i < size; i++) {
-                    if (prices[i] == 30) {
-                        prices[i] = 0;
-                        countB--;
-                        removed++;
-                    }
-                    if (removed == 2) {
-                        prices[i] = 45;
-                        break;
-                    }
-                }
-            }
+        //     while (countB >= 2) {
+        //         int removed = 0;
+        //         for (int i = 0; i < size; i++) {
+        //             if (prices[i] == 30) {
+        //                 prices[i] = 0;
+        //                 countB--;
+        //                 removed++;
+        //             }
+        //             if (removed == 2) {
+        //                 prices[i] = 45;
+        //                 break;
+        //             }
+        //         }
+        //     }
 
-        while (countF >= 3) {
-            for (int i = 0; i < size; i++) {
-                if (prices[i] == 10 && countF >= 3) {
-                    prices[i] = 0;
-                    countF -= 3;
-                }
-            }
-        }
+        // while (countF >= 3) {
+        //     for (int i = 0; i < size; i++) {
+        //         if (prices[i] == 10 && countF >= 3) {
+        //             prices[i] = 0;
+        //             countF -= 3;
+        //         }
+        //     }
+        // }
 
-        int sum = 0;
+        // int sum = 0;
 
-        for (int i = 0; i < size; i++) {
-            sum += prices[i];
-        }
+        // for (int i = 0; i < size; i++) {
+        //     sum += prices[i];
+        // }
 
-        return sum;
+        // return sum;
+        return 0;
     }
 
     //     public static void main(String [] args) {
