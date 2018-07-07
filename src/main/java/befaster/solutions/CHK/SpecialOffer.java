e48f0bf9c40924d4b1d162695695c80f0a62ddfb
sbx_user1061 <sbx_user1061@ip-10-12-248-210.eu-west-2.compute.internal>
2018-07-07 16:33:52 +0000

    Sat Jul 07 16:33:52 UTC 2018

--- src/main/java/befaster/solutions/CHK/SpecialOffer.java
+++ src/main/java/befaster/solutions/CHK/SpecialOffer.java
@@ -2,6 +2,17 @@ public class SpecialOffer {
 
     char itemType;
 
-    public void itemAOffer()
+    public int itemAOffer(int price, int itemsNumber) {
+
+        while (itemsNumber >= 5) {
+            price -= 50;
+            itemsNumber -= 5;
+        }
+
+        while (itemsNumber >= 3) {
+            price -= 20;
+            itemsNumber -= 3;
+        }
+    }
 
 }
